3402704af5963b332d76f2797b51329bec0485b4
MickMake <github@mickmake.com>
2022-02-07 20:30:42 +1100

    Update ModbusWeb.py

--- ModbusWeb.py
+++ ModbusWeb.py
@@ -135,7 +135,7 @@ client.close()
 logging.info("Modbus connected")
 
 # Configure MQTT
-mqtt_client = mqtt.Client("ModbusTCP")
+mqtt_client = mqtt.Client("ModbusWeb")
 mqtt_client.username_pw_set(options['mqtt_user'], options['mqtt_pass'])
 if options['mqtt_port'] == 8883:
     mqtt_client.tls_set()
